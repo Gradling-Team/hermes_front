491512f4eadda7b4344cf48fed472f5a67348656
MathieuChntt <mathieu.chantot@efrei.net>
2022-10-04 16:18:55 -0400

    Setup du home et du login

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/logo.png" alt="logo">
+        </div>
+        <div class="Hermes">
+            <router-link>><a href="#">Hermes</a></router-link>
+        </div>
+        <div class="login">
+            <button>Login</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    components: {
+    }
+
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.logo{
+    display: inline-block;
+    width: 10em;
+    height: 10em;
+}
+img{
+    display: block;
+    width: 90%;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<!--Login page-->
+<template>
+    <div class="login">
+        <Header></Header>
+        <div class="login-form">
+        <h1>Login</h1>
+        <form>
+            <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
